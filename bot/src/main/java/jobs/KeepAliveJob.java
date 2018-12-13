2cc9283167987c2ade2423c6a3ef1b60a86430fd
Landon Patmore <landon.patmore@gmail.com>
2018-12-13 15:45:35 -0500

    added logging for keep alive

--- bot/src/main/java/jobs/KeepAliveJob.java
+++ bot/src/main/java/jobs/KeepAliveJob.java
@@ -1,12 +1,19 @@
 package jobs;
 
 import com.mashape.unirest.http.Unirest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
+import utils.JobRunner;
 
 public class KeepAliveJob implements Job {
+    private static final Logger log = LogManager.getLogger(JobRunner.class);
+
     @Override
     public void execute(JobExecutionContext context) {
+        log.debug("Keeping server alive...");
         Unirest.get("http://localhost:4567/keepalive");
+        log.debug("Server kept alive...");
     }
 }
