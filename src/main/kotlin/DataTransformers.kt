ee2d9fe957a6b6e54d8eaf02ac50bbd2f276bf1e
Landon Patmore <landon.patmore@gmail.com>
2019-08-10 20:55:07 -0400

    Moved to Kotlin
    - also moved to extension functions to transform messages coming from fantasy service on the stream

---
+++ src/main/kotlin/DataTransformers.kt
@@ -0,0 +1,158 @@
+import io.reactivex.Observable
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.lang.StringBuilder
+import java.time.Instant
+import java.time.LocalDateTime
+
+
+fun Observable<Document>.toTransaction(): Observable<Element> =
+    flatMapIterable {
+        it.select("transaction")
+    }
+
+fun Observable<Element>.toMessageEvent(): Observable<MessageEvent?> =
+    map {
+        when (it.select("type").first().text()) {
+            "add" -> MessageEvent.Add(it)
+            "drop" -> MessageEvent.Drop(it)
+            "add/drop" -> MessageEvent.AddDrop(it)
+            "trade" -> MessageEvent.Trade(it)
+            "commish" -> MessageEvent.Commish(it)
+            else -> null
+        }
+    }
+
+fun Observable<MessageEvent?>.toMessage(): Observable<String> =
+    map {
+        when (it) {
+            is MessageEvent.Add -> addMessage(it.event)
+            is MessageEvent.Drop -> dropMessage(it.event)
+            is MessageEvent.AddDrop -> addDropMessage(it.event)
+            is MessageEvent.Trade -> tradeMessage(it.event)
+            is MessageEvent.Commish -> commishMessage()
+        }
+    }
+
+private fun addMessage(event: Element): String {
+    val timestamp = event.select("timestamp").text().toLong()
+    val fantasyTeam = event.select("destination_team_name").text()
+    val players = event.select("player")
+
+    val playersAdded = StringBuilder()
+
+    for (player: Element in players) {
+        val name = player.select("full").text()
+        val nflTeam = player.select("editorial_team_abbr").text()
+        val position = player.select("display_position").text()
+
+        playersAdded.append("$name ($nflTeam, $position), ")
+    }
+
+    val finalMessage = playersAdded.trimEnd().removeSuffix(",")
+
+    return "ADD ALERT\n\n" +
+            "Team: $fantasyTeam\n" +
+            "${if (players.size > 1) "Players" else "Player"}: $finalMessage\n" +
+            "Time: $timestamp"
+}
+
+private fun dropMessage(event: Element): String {
+    val timestamp = event.select("timestamp").text()
+    val fantasyTeam = event.select("source_team_name").text()
+    val players = event.select("player")
+
+    val playersDropped = StringBuilder()
+
+
+    for (player: Element in players) {
+        val name = player.select("full").text()
+        val nflTeam = player.select("editorial_team_abbr").text()
+        val position = player.select("display_position").text()
+
+        playersDropped.append("$name ($nflTeam, $position), ")
+    }
+
+    val finalMessage = playersDropped.trimEnd().removeSuffix(",")
+
+    return "DROP ALERT\n\n" +
+            "Team: $fantasyTeam\n" +
+            "${if (players.size > 1) "Players" else "Player"}: $finalMessage\n" +
+            "Time: $timestamp"
+}
+
+private fun addDropMessage(event: Element): String {
+    val timestamp = event.select("timestamp").text()
+    val fantasyTeam = event.select("source_team_name").text()
+    val players = event.select("player")
+
+    val playersAdded = StringBuilder()
+    val playersDropped = StringBuilder()
+
+    var playersAddedCount = 0
+    var playersDroppedCount = 0
+
+    for (player: Element in players) {
+        val name = player.select("full").text()
+        val nflTeam = player.select("editorial_team_abbr").text()
+        val position = player.select("display_position").text()
+
+        val e = "$name ($nflTeam, $position), "
+
+        if (player.select("type").text() == "add") {
+            playersAdded.append(e)
+            playersAddedCount++
+        } else {
+            playersDropped.append(e)
+            playersDroppedCount++
+        }
+    }
+
+    val finalMessageAdded = playersAdded.trimEnd().removeSuffix(",")
+    val finalMessageDropped = playersDropped.trimEnd().removeSuffix(",")
+
+    return "ADD/DROP ALERT\n\n" +
+            "Team: $fantasyTeam\n" +
+            "Added ${if (playersAddedCount > 1) "Players" else "Player"}: $finalMessageAdded\n" +
+            "Dropped ${if (playersDroppedCount > 1) "Players" else "Player"}: $finalMessageDropped\n" +
+            "Time: $timestamp"
+}
+
+private fun tradeMessage(event: Element): String {
+    val timestamp = event.select("timestamp").text()
+
+    val trader = event.select("trader_team_name").text()
+    val tradee = event.select("tradee_team_name").text()
+
+    val players = event.select("player")
+
+    val fromTraderTeam = StringBuilder()
+    val fromTradeeTeam = StringBuilder()
+
+    for (player: Element in players) {
+        val fantasyTeam = player.select("source_team_name").text()
+        val name = player.select("full").text()
+        val nflTeam = player.select("editorial_team_abbr").text()
+        val position = player.select("display_position").text()
+
+        val e = "$name ($nflTeam, $position), "
+
+        if (fantasyTeam == trader) {
+            fromTraderTeam.append(e)
+        } else {
+            fromTradeeTeam.append(e)
+        }
+    }
+
+    val finalMessageFromTrader = fromTraderTeam.trimEnd().removeSuffix(",")
+    val finalMessageFromTradee = fromTradeeTeam.trimEnd().removeSuffix(",")
+
+    return "TRADE ALERT\n\n" +
+            "Time: $timestamp\n" +
+            "$trader received: $finalMessageFromTradee\n" +
+            "$tradee received: $finalMessageFromTrader"
+}
+
+private fun commishMessage(): String {
+    return "The commissioner has changed a setting within the league.  You may want to check or ask them what they changed!"
+}
