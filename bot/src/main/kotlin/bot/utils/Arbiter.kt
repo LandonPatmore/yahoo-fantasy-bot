dcbbe99a14433120842a969e1753b63a1f9c54bd
Landon Patmore <landon.patmore@gmail.com>
2019-09-05 15:44:53 -0400

    fixed job timing issue

--- bot/src/main/kotlin/bot/utils/Arbiter.kt
+++ bot/src/main/kotlin/bot/utils/Arbiter.kt
@@ -111,7 +111,7 @@ object Arbiter {
         JobRunner.createJob(ScoreUpdateJob::class.java, "0 55 3 ? 9-1 FRI *")
         JobRunner.createJob(ScoreUpdateJob::class.java, "0 00 17 ? 9-1 SUN *")
         JobRunner.createJob(ScoreUpdateJob::class.java, "0 00 20 ? 9-1 SUN *")
-        JobRunner.createJob(ScoreUpdateJob::class.java, "0 00 0 ? 9-1 SUN *")
+        JobRunner.createJob(ScoreUpdateJob::class.java, "0 00 0 ? 9-1 MON *")
         JobRunner.createJob(ScoreUpdateJob::class.java, "0 55 3 ? 9-1 MON *")
         JobRunner.createJob(ScoreUpdateJob::class.java, "0 55 3 ? 9-1 TUE *")
 
