468bc5c50fd63bbd22daafb53016da68d8356425
Lauren Bosak <lbo@slack-corp.com>
2019-09-14 15:11:32 -0700

    Better formatted Transaction messages

--- bot/src/main/kotlin/bot/transformers/MessageTransformer.kt
+++ bot/src/main/kotlin/bot/transformers/MessageTransformer.kt
@@ -6,11 +6,11 @@ import bot.messaging_services.Message
 fun Observable<Message>.convertToStringMessage(): Observable<String> =
     map {
         when (it) {
-            is Message.Transaction.Add -> createMessage("===ADD ALERT===", it)
-            is Message.Transaction.Drop -> createMessage("===DROP ALERT===", it)
-            is Message.Transaction.AddDrop -> createMessage("===ADD/DROP ALERT===", it)
-            is Message.Transaction.Trade -> createMessage("===TRADE ALERT===", it)
-            is Message.Transaction.Commish -> createMessage("===COMMISH ALERT===", it)
+            is Message.Transaction.Add -> createMessage(":mega: <b>ADD ALERT</b> :chart_with_upwards_trend:\\n━━━━━━━━━", it)
+            is Message.Transaction.Drop -> createMessage(":mega: <b>DROP ALERT</b> :chart_with_downwards_trend:\\n━━━━━━━━━━", it)
+            is Message.Transaction.AddDrop -> createMessage(":mega: <b>ADD/DROP ALERT</b> :chart_with_upwards_trend::chart_with_downwards_trend:\\n━━━━━━━━━━━━━━", it)
+            is Message.Transaction.Trade -> createMessage(":mega: <b>TRADE ALERT</b> :twisted_rightwards_arrows:\\n━━━━━━━━━━", it)
+            is Message.Transaction.Commish -> createMessage(":mega: <b>COMMISH ALERT</b> :raising_hand:\\n━━━━━━━━━━", it)
             is Message.Standings -> createMessage(null, it)
             is Message.Score -> createMessage(null, it)
             is Message.CloseScore -> createMessage(null, it)
@@ -24,6 +24,6 @@ fun Observable<Message>.convertToStringMessage(): Observable<String> =
 
 private fun createMessage(alertTitle: String?, message: Message): String {
     return alertTitle?.let {
-        "$alertTitle\\n\\n${message.message}"
+        "$alertTitle\\n${message.message}"
     } ?: message.message
 }
